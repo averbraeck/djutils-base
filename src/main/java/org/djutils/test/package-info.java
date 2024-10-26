9f52892dfcd60970659e6daeb5b2307da9e1808c
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-10-26 02:45:47 +0200

    Issue #10. Create generic ExceptionTest class

---
+++ src/main/java/org/djutils/test/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Helper classes for unit tests in projects.
+ * <p>
+ * Copyright (c) 2024-2024 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
+ * distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+package org.djutils.test;
