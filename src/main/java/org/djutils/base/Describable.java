c6f58507c7542de9cdeb254b191b5f3036f4b679
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-11-01 19:41:10 +0100

    Issue #14. Add Describable interface with getDescription()

---
+++ src/main/java/org/djutils/base/Describable.java
@@ -0,0 +1,23 @@
+package org.djutils.base;
+
+/**
+ * Interface for all classes with a getDescription() method in addition to a getId() method. <br>
+ * <br>
+ * Copyright (c) 2013-2024 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
+ * distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>. <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public interface Describable extends Identifiable
+{
+
+    /**
+     * Returns the description.
+     * @return description.
+     */
+    String getDescription();
+
+}
