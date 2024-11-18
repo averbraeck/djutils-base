86702d7343728c8701b6e3429de6fc4fe1fd6dbc
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-11-18 23:32:09 +0100

    Update copyright year to 2024 on some Java files

--- src/test/java/org/djutils/base/IdentifiableTest.java
+++ src/test/java/org/djutils/base/IdentifiableTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 /**
  * Testing of Identifiable and Describable interfaces.
  * <p>
- * Copyright (c) 2023-2023 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * Copyright (c) 2024-2024 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
  * distributed under a three-clause BSD-style license, which can be found at
  * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>.
