511fbea10f0adcc94b3bf434b43bd0c475845fa0
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-10-19 12:28:30 +0200

    Issue #34. First unit tests for ResourceResolver class

---
+++ src/test/java/org/djutils/io/ResourceResolverTest.java
@@ -0,0 +1,160 @@
+package org.djutils.io;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+
+import org.djutils.test.UnitTest;
+import org.junit.jupiter.api.Test;
+
+/**
+ * ResourceResolverTest tests the ResourceResolver class.
+ * <p>
+ * Copyright (c) 2025-2025 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
+ * distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank">Alexander Verbraeck</a>
+ */
+public class ResourceResolverTest
+{
+    /**
+     * Test whether ResourceResolver retrieves files.
+     * @throws IOException on I/O error
+     */
+    @Test
+    public final void fileTest() throws IOException
+    {
+        // create a temporary file.
+        Path tempFile = Files.createTempFile("filetest-", ".temp");
+        tempFile.toFile().deleteOnExit();
+        var res1 = ResourceResolver.resolve(tempFile.toAbsolutePath().toString());
+        assertNotNull(res1);
+        assertEquals(tempFile, res1.asPath());
+        
+        // non-existent file
+        UnitTest.testFail(() -> ResourceResolver.resolve(tempFile.toAbsolutePath().toString() + ".xyz"));
+    }
+
+    /**
+     * Test whether ResourceResolver retrieves resources.
+     * @throws IOException on I/O error
+     * @throws URISyntaxException on URL error
+     */
+    @Test
+    public final void resourceTest() throws IOException, URISyntaxException
+    {
+        String path1 = "/org/djutils-test-resources/test.txt";
+        List<String> lines = Files.readAllLines(ResourceResolver.resolve(path1).asPath());
+        assertEquals(3, lines.size());
+        assertEquals("abc", lines.get(0));
+
+        String path2 = "org/djutils-test-resources/test.txt";
+        lines = Files.readAllLines(ResourceResolver.resolve(path2).asPath());
+        assertEquals(3, lines.size());
+        assertEquals("def", lines.get(1));
+
+        String path3 = "/org/djutils-test-resources/test.txt";
+        lines = Files.readAllLines(ResourceResolver.resolveAsResource(path3).asPath());
+        assertEquals(3, lines.size());
+        assertEquals("def", lines.get(1));
+
+        String path4 = "org/djutils-test-resources/test.txt";
+        lines = Files.readAllLines(ResourceResolver.resolveAsResource(path4).asPath());
+        assertEquals(3, lines.size());
+        assertEquals("def", lines.get(1));
+        
+        UnitTest.testFail(() -> ResourceResolver.resolveAsResource("/org/abc.xyz").asPath());
+        UnitTest.testFail(() -> ResourceResolver.resolveAsResource("org/abc.xyz").asPath());
+    }
+
+    /**
+     * Test whether ResourceResolver can open a stream.
+     * @throws IOException on I/O error
+     * @throws URISyntaxException on URL error
+     */
+    @Test
+    public final void resourceAsStreamTest() throws IOException, URISyntaxException
+    {
+        InputStream stream = ResourceResolver.resolve("/org/djutils-test-resources/test.txt").openStream();
+        byte[] barr = stream.readAllBytes();
+        stream.close();
+        assertEquals('a', barr[0]);
+
+        UnitTest.testFail(() -> (ResourceResolver.resolve("xxx:///org::djutils-test-resources<>test.txt").openStream()));
+        UnitTest.testFail(() -> ResourceResolver.resolve("/org/djutils-test-resources/test123.txt").openStream());
+    }
+
+    /**
+     * Test whether ResourceResolver retrieves entries from jar-files.
+     * @throws IOException on I/O error
+     * @throws URISyntaxException on error
+     */
+    @Test
+    public final void jarTest() throws IOException, URISyntaxException
+    {
+        // create a temporary jar file.
+        File jarFile = File.createTempFile("filetest-", ".jar");
+        jarFile.deleteOnExit();
+        File file1 = File.createTempFile("filetest-", ".f1");
+        file1.deleteOnExit();
+        File file2 = File.createTempFile("filetest-", ".f2");
+        file2.deleteOnExit();
+        File file3 = File.createTempFile("filetest-", ".f3");
+        file3.deleteOnExit();
+
+        FileOutputStream fos = new FileOutputStream(jarFile.getAbsolutePath());
+        JarOutputStream jos = new JarOutputStream(fos);
+        addToJarFile(file1.getAbsolutePath(), jos);
+        addToJarFile(file2.getAbsolutePath(), jos);
+        addToJarFile(file3.getAbsolutePath(), jos);
+        jos.close();
+        fos.close();
+
+        String jarFilePath = jarFile.getAbsolutePath();
+        URL jarURL = ResourceResolver.resolve(jarFilePath).asUrl();
+        assertNotNull(jarURL);
+        System.out.println(jarFile.getAbsolutePath() + "!/" + file1.getName());
+        URL jar1 = ResourceResolver.resolve(jarFile.getAbsolutePath() + "!/" + file1.getName()).asUrl();
+        System.out.println(jar1.toURI());
+        assertNotNull(jar1);
+    }
+
+    /**
+     * Copy a plain file into a jar file.
+     * @param fileName name of the file to copy
+     * @param jos stream for writing into the jar file
+     * @throws FileNotFoundException when the input file could not be found
+     * @throws IOException when the input file could not be read, or writing to the jar stream fails
+     */
+    public void addToJarFile(final String fileName, final JarOutputStream jos) throws FileNotFoundException, IOException
+    {
+        File file = new File(fileName);
+        FileInputStream fis = new FileInputStream(file);
+        JarEntry zipEntry = new JarEntry(file.getName());
+        jos.putNextEntry(zipEntry);
+        byte[] bytes = new byte[1024];
+        int length;
+        while ((length = fis.read(bytes)) >= 0)
+        {
+            jos.write(bytes, 0, length);
+        }
+        jos.closeEntry();
+        fis.close();
+    }
+
+}
